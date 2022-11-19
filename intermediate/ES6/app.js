f141fbd7c023d996777f0e08921beae6ef0e7033
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-11-19 20:26:47 +0200

    es6

--- intermediate/ES6/app.js
+++ intermediate/ES6/app.js
@@ -11,3 +11,22 @@ class Elf {
 
     }
 }
+
+
+//eg
+const player = "Bobby";
+let experience = 100;
+let wizardLevel = false;
+if (experience < 90) {
+    let wizardLevel = true;
+    console.log(wizardLevel);
+}
+console.log(wizardLevel);
+
+//eg
+const player2 = obj.player2;
+const experience2 = obj.experience;
+let wizardLevel2 = obj.wizardLevel2;
+// or
+// const { player2, experience2 } = obj;
+// let wizardLevel2 = obj;
