9a8b69ad8fe6c37e430bf5e55f73a93f745f8780
Alexandra2888 <moldovan.alexandra28@gmail.com>
2022-11-30 16:31:05 +0200

    hoisting

--- basic/hoisting/README.md
+++ basic/hoisting/README.md
@@ -1,12 +1,17 @@
 ## hoisting
 = the behaviour of moving the variables/ function declaratiosn to the top of their respective environment during compilation.
+= the entire process of compilation / parsing and code execution
 
-## eg
+! declaration using let & const hoist in a different way ! (in doc MDN says they don't hoist but it's innacurate)
 
-console.log('1');
-console.log('teddy');
-let teddy = 'bear';
-function sing(){
-    console.log('la laa');
-}
- -> 1, undefined, la laa
+## In the compilation stage we want to create a plan/blueprint for formal declarations and the scopes that they belong to.
+
+## In the execution stage we know what are our identifiers and we know what scope do they belong to -> we access these identifiers and we want to find out wehetever they are in target reference or they are in the source reference. If tehy are in target reference -> grab the value of that and allocate spoace in memory to it. If we are in source posiiton -> takes us all way to where that source points.
+
+
+## Temporal dead zone (TDZ)
+* lexical declaration -> let bindingIdentifier(lexicalBinding) = AssignmentExpression
+* lexical declaration -> const bindingIdentifier(lexicalBinding) = AssigningExpression
+
+* var -> originally it's in an undefined state
+* let/const -> originally in an undefined state
---
+++ basic/hoisting/app.js
@@ -0,0 +1,93 @@
+// eg
+
+console.log('1');
+console.log('teddy');
+let teddy = 'bear';
+function sing() {
+    console.log('la laa');
+}
+// 1, undefined, la laa
+
+
+// 1. variable hoisting
+//our regualr code:
+console.log(book);
+console.log(movie);
+
+book;
+movie;
+
+console.log(book);
+console.log(movie);
+
+var book = "Dune";
+var movie = "The Movie Dune";
+
+console.log(book);
+console.log(movie);
+
+
+//the hoisting miosconception
+// var book;
+// var movie;
+
+// console.log(book);
+// console.log(movie);
+
+// book;
+// movie;
+
+// console.log(book);
+// console.log(movie);
+
+//  book = "Dune";
+//  movie = "The Movie Dune";
+
+// console.log(book);
+// console.log(movie);
+
+//2. function hoisting
+
+book();
+movie(); // typeError
+function book() {
+    console.log("The Great Gatsby");
+}
+
+var movie = function () {
+    console.log("The Good, the Bad and the Ugly");
+};
+movie() // no error
+
+
+//the hoisting misconception for function declaration
+function book() {
+    console.log("The Great Gatsby");
+};
+book();
+
+//the hoisting misconception for function expression
+
+ movie = function () {
+    console.log("The Good, the Bad and the Ugly");
+};
+movie()
+
+
+//proof on hoisting of let & const
+var name = "Chadwick";
+
+{
+    console.log(name);
+    let name = "Rosie";
+}
+
+// const number;
+// number = 1;
+// number = 2;
+// console.log(number);
+
+// let number;
+// let number = 1;
+//      number = 3;
+// console.log(number);
