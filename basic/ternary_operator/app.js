18156d4a719858e23778f577f35e9f36130953a7
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-01-18 20:25:52 +0200

    ternary operator

--- basic/ternary_operator/app.js
+++ basic/ternary_operator/app.js
@@ -6,4 +6,19 @@ score === 10 ? console.log('Grade A') : console.log('Grade B');
 let answer = isUserValid(true) ? "You can enter" : "Access denied";
 
 //eg
-let automatedAnswer = "Your account is " + (isUserValid(false) ? "1234" : "not available")
+let automatedAnswer = "Your account is " + (isUserValid(false) ? "1234" : "not available");
+
+//eg
+function isUserValid(bool) {
+return bool
+}
+ 
+//eg:
+function condition() {
+    if(isUserValid(true)) {
+        return "You can enter"
+    } else {
+        return "Access denied!"
+    }
+}
+let answer2 = condition();
