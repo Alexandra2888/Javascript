f614a77c6b786baa85762814292931c26063dccf
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-13 18:11:56 +0200

    strings

--- basic/string_methods/app.js
+++ basic/string_methods/app.js
@@ -47,3 +47,53 @@ console.log(movie.includes('a'));
 
 // replace
 console.log(movie.replace('emento', 'agento'));
+
+
+
+// String properties and methods
+// wrapper String Object, don't memorize methods
+let text = ' Peter Jordan';
+let result = text.length;
+console.log(result);
+
+console.log(text.length);
+console.log(text.toLowerCase());
+console.log(text.toUpperCase());
+console.log(text.charAt(0));
+console.log(text.charAt(text.length - 1));
+console.log(text.indexOf('e'));
+console.log(text);
+console.log(text.trim());
+console.log(text.trim().toLowerCase().startsWith('peter'));
+console.log(text.includes('eter'));
+console.log(text.slice(0, 2));
+console.log(text.slice(-3));
+
+// const person = {
+//   name: 'peter', // property
+//   greeting() {
+//     // method
+//     console.log("Hey, I'm Peter");
+//   },
+// };
+
+// console.log(person);
+
+// console.log(person.name);
+// person.greeting();
+
+
+// Template Literals - ES6+
+// Backtick characters `` - above tab (left from one)
+// Interpolation ${} - insert expression(value)
+
+const name = 'john';
+const age = 25;
+const sentence = "Hey it's " + name + ' and he is ' + age + ' years old';
+
+const value = `Hey it's ${name} and he is ${age} years old. And here is some simple math ${
+  4 + 4
+}`;
+console.log(value);
+
+console.log(sentence);
