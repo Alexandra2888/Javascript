7931e5ebb7aabf7b7a55d9bf8c0e0aa12c965d38
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-13 16:02:43 +0200

    objects

--- basic/this/script.js
+++ basic/this/script.js
@@ -50,3 +50,64 @@ var d = {
 d.say()()
 // {name: 'jay', say[Function]}
 
+
+// this
+// points to the left of the dot
+
+const john = {
+  firstName: 'john',
+  lastName: 'anderson',
+  fullName: function () {
+    console.log(this);
+    console.log(`My full name is ${this.firstName} ${this.lastName}`);
+  },
+};
+const bob = {
+  firstName: 'peter',
+  lastName: 'sanders',
+  fullName: function () {
+    console.log(this);
+    console.log(`My full name is ${this.firstName} ${this.lastName}`);
+  },
+};
+
+john.fullName();
+bob.fullName();
+
+
+/* In Reg Functions (not arrow) "this"
+determined by "HOW"!!! a function is invoked (left of .)
+
+defaults to global - window
+arrow functions - pump the breaks
+*/
+
+// console.log(this);
+
+function showThis() {
+  console.log(this);
+}
+
+const john1 = {
+  name: 'john1',
+  showThis: showThis,
+};
+const bob1 = {
+  name: 'bob1',
+  showThis: showThis,
+};
+
+john.showThis();
+bob.showThis();
+
+showThis();
+const btn1 = document.querySelector('.btn-1');
+const btn2 = document.querySelector('.btn-2');
+
+btn1.addEventListener('click', showThis);
+btn2.addEventListener('click', showThis);
+btn2.addEventListener('click', function () {
+  showThis();
+});
+
+
--- intermediate/objects/script.js
+++ intermediate/objects/script.js
@@ -312,3 +312,128 @@ console.log(person['name']);
 console.log(person['random-value']);
 
 console.log(person[random]);
+
+
+// Blue Print
+// Factory Functions and Constructor Functions
+// Factory Functions
+
+// const john = {
+//   firstName: 'john',
+//   lastName: 'anderson',
+//   fullName: function () {
+//     console.log(
+//       `My full name is ${this.firstName} ${this.lastName} and I love JS`
+//     );
+//   },
+// };
+// const bob = {
+//   firstName: 'peter',
+//   lastName: 'sanders',
+//   fullName: function () {
+//     console.log(`My full name is ${this.firstName} ${this.lastName}`);
+//   },
+// };
+
+function createPerson(firstName, lastName) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    fullName: function () {
+      console.log(
+        `My full name is ${this.firstName} ${this.lastName} and I love JS`
+      );
+    },
+  };
+}
+
+const john = createPerson('john', 'anderson');
+john.fullName();
+const bob = createPerson('susy', 'apple');
+bob.fullName();
+const susy = createPerson('bob', 'jordan');
+susy.fullName();
+
+
+// Blue Print
+// Factory Functions and Constructor Functions
+// Constructor Functions
+// new - creates new object, points to it, omit return
+
+function Person(firstName, lastName) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.fullName = function () {
+    console.log(
+      `My full name is ${this.firstName} ${this.lastName} and I love React`
+    );
+  };
+  console.log(this);
+}
+const john1 = new Person('john1', 'anderson');
+john.fullName();
+const bob1 = new Person('bob1', 'jordan');
+bob.fullName();
+
+function createPerson(firstName, lastName) {
+  return {
+    firstName: firstName,
+    lastName: lastName,
+    fullName: function () {
+      console.log(
+        `My full name is ${this.firstName} ${this.lastName} and I love JS`
+      );
+    },
+  };
+}
+
+
+/*
+Prototypal Inheritance Model
+Javascript uses prototypal inheritance model. That means that every constructor function/class has a prototype property that is shared by every instance of the constructor/class. So any properties and methods or prototype can be acessed by every instance. prototype property returns a object
+*/
+
+function Account(name, initialBalance) {
+  this.name = name;
+  this.balance = initialBalance;
+}
+
+const john2 = new Account('john2', 200);
+const bob2 = new Account('bob2', 0);
+
+Account.prototype.bank = 'CHASE';
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+  console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+};
+console.log(john2.bank);
+console.log(bob2);
+
+john.deposit(300);
+bob.deposit(1000);
+
+
+/*
+Property Lookup
+If child does not have ask parent
+Everything in JS is an Object
+*/
+
+function Account(name, initialBalance) {
+  this.name = name;
+  this.balance = initialBalance;
+  // this.bank = 'Bank Of America';
+}
+
+const john3 = new Account('john3', 200);
+const bob3 = new Account('bob3', 0);
+
+Account.prototype.bank = 'CHASE';
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+  console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+};
+// console.log(john.bank);
+console.log(bob3);
+console.log({});
+console.log([]);
