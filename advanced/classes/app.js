7618eeb9495d856f1aff98aa6c9bcdd9622a9c54
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-13 16:04:05 +0200

    classes

--- advanced/classes/app.js
+++ advanced/classes/app.js
@@ -44,3 +44,52 @@ class anotherWorkshop {
 
 let react = new AnotherWorkshop("Trevor");
 react.ask("teaches react???")
+
+
+class Account {
+  constructor(name, initialBalance) {
+    this.name = name;
+    this.balance = initialBalance;
+  }
+  bank = 'Chase';
+  deposit(amount) {
+    this.balance += amount;
+    console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+  }
+}
+
+const john = new Account('john', 0);
+console.log(john);
+console.log(john.name);
+john.deposit(500);
+console.log(john.bank);
+
+const bob = new Account('bob', 700);
+console.log(bob);
+console.log(bob.name);
+bob.deposit(1000);
+console.log(bob.bank);
+
+
+/*
+ES6 Classes - Syntactic Sugar
+Prototypal Inheritance
+
+*/
+
+function Account(name, initialBalance) {
+  this.name = name;
+  this.balance = initialBalance;
+  // this.bank = 'Bank Of America';
+}
+
+const john1 = new Account('john1', 200);
+const bob1 = new Account('bob1', 0);
+
+Account.prototype.bank = 'CHASE';
+Account.prototype.deposit = function (amount) {
+  this.balance += amount;
+  console.log(`Hello ${this.name}, your balance is ${this.balance}`);
+};
+console.log(john.bank);
+console.log(bob);
