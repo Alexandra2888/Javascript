337d6f33d87394cc41da852d992abd6ba8e64f43
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2023-03-13 15:56:06 +0200

    iife, hoisting, closures

--- advanced/closures&prototypal_inheritance/app.js
+++ advanced/closures&prototypal_inheritance/app.js
@@ -359,3 +359,80 @@ for (let i = 1; i <= 3; i++) {
         console.log(`i: ${i}`);
     }, i * 1000);
 }
+
+
+
+// Closure
+// closure gives you an access to an outer function's scope from an inner function
+// make private variables with closures
+
+function outer() {
+  let privateVar = 'secret';
+  function inner() {
+    console.log(`hello there secret is : ${privateVar} `);
+  }
+  return inner;
+  inner();
+}
+// console.log(privateVar);
+
+outer()();
+
+const value = outer();
+console.log(value);
+value();
+
+
+// Closure
+
+function newAccount(name, initialBalance) {
+  let balance = initialBalance;
+  function showBalance() {
+    console.log(`Hey ${name}, your balance is ${balance}`);
+  }
+  return showBalance;
+}
+
+newAccount('susan', 500)();
+
+const john = newAccount('john', 300);
+const bob = newAccount('bob', 1000);
+
+john();
+bob();
+
+
+
+// Closure
+
+function newAccount(name, initialBalance) {
+  let balance = initialBalance;
+  function showBalance() {
+    console.log(`Hey ${name}, your balance is ${balance}`);
+  }
+  function deposit(amount) {
+    balance += amount;
+    showBalance();
+  }
+  function withdraw(amount) {
+    if (amount > balance) {
+      console.log(`Hey, ${name}, sorry not enough funds`);
+      return;
+    }
+    balance -= amount;
+    showBalance();
+  }
+  return { showBalance: showBalance, deposit: deposit, withdraw: withdraw };
+}
+
+const john1 = newAccount('john1', 1000);
+const bob1 = newAccount('bob1', 1000);
+
+john.showBalance();
+john.deposit(400);
+john.deposit(1000);
+john.withdraw(400);
+john.balance = 10000;
+john.withdraw(2001);
+bob.showBalance();
+bob.deposit(400);
--- basic/hoisting/app.js
+++ basic/hoisting/app.js
@@ -91,3 +91,28 @@ var name = "Chadwick";
 // let number = 1;
 //      number = 3;
 // console.log(number);
+
+
+// Hoisting
+// function and var declarations are hoisted
+// safer to access only after initialized
+display();
+moreComplex();
+console.log(display);
+console.log(moreComplex);
+// console.log(firstName);
+// console.log(lastName);
+// console.log(random);
+
+const firstName = 'john';
+let lastName = 'jordan';
+var random = 'random';
+
+function display() {
+  console.log('hello world');
+}
+
+function moreComplex() {
+  console.log(` ${random}`);
+}
+
